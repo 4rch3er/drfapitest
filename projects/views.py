db334c2d67c093d8c37df8d9b750d26461b7b7eb
4rch3er <144876515+4rch3er@users.noreply.github.com>
2024-11-23 01:51:13 -0300

    Update views.py

--- projects/views.py
+++ projects/views.py
@@ -8,3 +8,7 @@ from .serializers import AnimeSerializer
 class AnimeViewSet(viewsets.ModelViewSet):
     queryset = Anime.objects.all()
     serializer_class = AnimeSerializer
+
+    def create(self, request, *args, **kwargs):
+        print("Datos recibidos:", request.data)  # Para verificar los datos enviados
+        return super().create(request, *args, **kwargs)

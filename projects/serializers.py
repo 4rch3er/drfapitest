e4470bd3c3b08dc78be9780cb1c42faab0ed104d
4rch3er <144876515+4rch3er@users.noreply.github.com>
2024-11-27 07:51:08 -0300

    Update serializers.py

--- projects/serializers.py
+++ projects/serializers.py
@@ -4,4 +4,5 @@ from .models import Anime
 class AnimeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Anime
-        fields = '__all__'
+        fields = ('id', 'titulo', 'tipo_anime', 'episodios', 'url', 'fecha', 'estado', 'generos', 'sinopsis')  # Lista de campos
+        #read_only_fields = ('id', ) 
